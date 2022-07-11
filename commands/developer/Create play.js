937a8b984b03f77f133b10ae88cb03ff553c7eae
Deglas <78974541+Deglas1@users.noreply.github.com>
2022-07-12 00:11:43 +0300

    Update Create play.js

--- commands/developer/Create play.js
+++ commands/developer/Create play.js
@@ -1,6 +1,8 @@
 module.exports = ({
-name: "create-play",
+name: "create-slash",
 code: `
 $createApplicationCommand[$guildid;play;Добавить трек в очередь;true;slash;{"name": "track", "description": "название трека", "type": 3, "required": true}]
+$createApplicationCommand[$guildid;skip; Пропустить трек;true;slash]
+$createApplicationCommand[$quildid;queue;Список очереди треков;true;slash]
 `
 })
