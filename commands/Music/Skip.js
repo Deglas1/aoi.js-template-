b5190e56fa5709b12969a7c11b5e9fee311225de
Deglas <78974541+Deglas1@users.noreply.github.com>
2022-07-12 00:07:34 +0300

    Update Skip.js

--- commands/Music/Skip.js
+++ commands/Music/Skip.js
@@ -4,4 +4,20 @@ type: "interaction",
 prototype: "slash",
 code: `$skip
 $interactionReply[Вы успешно пропустили трек]
-$onlyif[$queuelength
+$onlyif[$queuelength>=1;{
+"content": "Сейчас в очереди нет треков", 
+"ephemeral": true,
+"options": { "interaction": true }
+}]
+$onlyif[$voiceid[$clientid]!=;{
+"content": "Я не в ГС", 
+"ephemeral": true,
+"options": { "interaction": true }
+}]
+$onlyif[$voiceid!=;{
+"content": "Вы не в ГС", 
+"ephemeral": true,
+"options": { "interaction": true }
+}]
+`
+})
