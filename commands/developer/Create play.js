0a8605bb2a3567c1aaf18d926fcba969ce89787a
Deglas <78974541+Deglas1@users.noreply.github.com>
2022-07-11 23:52:14 +0300

    Create Create play.js

---
+++ commands/developer/Create play.js
@@ -0,0 +1,6 @@
+module.exports = ({
+name: "create-play",
+code: `
+$createApplicationCommand[$guildid;play;Добавить трек в очередь;true;slash;{"name": "track", "description": "название трека", "type": 3, "required": true}]
+`
+})
