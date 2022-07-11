2bfbf916ca504ee6e3f38a7bb9f513fb64a49ad5
Deglas <78974541+Deglas1@users.noreply.github.com>
2022-07-11 23:59:48 +0300

    Update Play.js

--- commands/Music/Play.js
+++ commands/Music/Play.js
@@ -1,5 +1,12 @@
 module.exports = ({
 name: "play",
+$if: "v4",
 type: "interaction",
 prototype: "slash",
 code: `$let[$playTrack[youtube;$nonEscape[$interactiondata[options.data[0].value]]]]
+$interactionReply[Вы добавили трек в очередь]
+$if[$hasPlayer==false]
+$joincVC
+$endif
+`
+})
