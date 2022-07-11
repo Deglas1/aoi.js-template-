3c84943657ac23e3b9d1d725de980d1f6d3dcb23
Deglas <78974541+Deglas1@users.noreply.github.com>
2022-07-11 23:49:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const aoijs = require("aoi.js")
 const bot = new aoijs.Bot({
    token: "токен бота",
    prefix: "$getservervar[prefix]",
-    intents: ["GUILDS", "GUILD_MESSAGES"]
+    intents: "all"
 })
 const loader = new aoijs.LoadCommands(bot)
 loader.load(bot.cmd,'./commands/') //загрузчик команд
@@ -11,3 +11,9 @@ bot.onInteractionCreate(); //для создания слешей и т.д
 bot.variables({
 prefix: "!"
 })
+const voice = new aoijs.Voice(bot, {
+cache: {
+    cacheType: "Memory",
+    enabled: true,
+  },
+}); 
